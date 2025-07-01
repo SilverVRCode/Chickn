02fe936786ecde4bbbfd13a68cbf5afe1e729f0f
Silvy :3c <101380293+SilverVRCode@users.noreply.github.com>
2025-06-30 21:45:43 -0400

    sidebar is now a component

--- chickn/src/main.js
+++ chickn/src/main.js
@@ -1,5 +1,18 @@
+import { createAuth0 } from '@auth0/auth0-vue';
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: "dev-b6rt0pxyylrinulb.us.auth0.com",
+    clientId: "Kk5Gdi5hg8vhwCKmKBoIRWlJEfpYKU3W",
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })
+);
+
+app.mount('#app');
